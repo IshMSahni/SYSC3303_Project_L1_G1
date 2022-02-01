491ad94dd8ea6500b43a48fd5f42ea3115f02fd3
mfurqan26 <muhammadfurqan@cmail.carleton.ca>
2022-02-01 15:34:44 -0500

    More intital code idea for scheduler

--- src/Scheduler_System.java
+++ src/Scheduler_System.java
@@ -10,9 +10,7 @@ public class Scheduler_System implements Runnable{
     private ArrayList<String> tasksQueue;
     private ArrayList<String> scheduledQueue;
 
-    /**
-     * Constructor for Scheduler_System
-     */
+    /** Constructor for Scheduler_System */
     public Scheduler_System(ArrayList<ElevatorCar> elevators, ArrayList<Floor> floors, Elevator_System elevator_system){
         this.elevators = elevators;
         this.elevator_system = elevator_system;
@@ -21,7 +19,30 @@ public class Scheduler_System implements Runnable{
         this.scheduledQueue = new ArrayList<>();
     }
 
+    /**Get Scheduled tasks from Scheduler*/
     public ArrayList<String> getScheduledQueue(){return this.scheduledQueue;}
+
+    /**Add task to queue for Scheduler*/
+    public void addToQueue(String task){tasksQueue.add(task);}
+
+    /**Remove task from all queues after finsihing a task*/
+    public void removeFromQueue(String task){
+        tasksQueue.remove(task);
+        scheduledQueue.remove(task);
+    }
+
+    /** Schedule tasks based off elevators & floors data */
+    public void scheduleTasks(){
+        //Get All Elevators position and status
+        ArrayList<Float> elevatorPositions = new ArrayList<>();
+        ArrayList<String> elevatorStatuses = new ArrayList<>();
+        for (int i = 0; i < elevators.size(); i++) {
+            elevatorPositions.add(elevators.get(i).getPosition());
+            elevatorStatuses.add(elevators.get(i).getStatus());
+        }
+
+        //Do some algorithm based off the data to schedule tasks
+    }
 
     @Override
     public void run() {
