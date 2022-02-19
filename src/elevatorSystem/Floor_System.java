384e0fb842c34d26970f5e97039761e5efb6c398
mfurqan26 <muhammadfurqan@cmail.carleton.ca>
2022-02-19 13:06:36 -0500

    some bug fixes

--- src/elevatorSystem/Floor_System.java
+++ src/elevatorSystem/Floor_System.java
@@ -44,6 +44,7 @@ public class Floor_System implements Runnable{
 		}
 		floors[lastFloorNumber] = new Floor(lastFloorNumber, totalNumElevators, this);
 		floors[lastFloorNumber].setFloor(lastFloorNumber,true);
+		allPeople = new ArrayList<>();
 	}
 
 	public void setScheduler_system(Scheduler_System system){
@@ -269,7 +270,6 @@ public class Floor_System implements Runnable{
 	public static void main(String[] args) {
 		int totalFloorNumber = 10; //total number of floors
 		int totalElevatorNumber = 1; //total number of elevators
-		allPeople = new ArrayList<Person>()
 
 		Floor_System floor_SubSystem = new Floor_System(totalFloorNumber,totalElevatorNumber);
 		Elevator_System elevator_SubSystem = new Elevator_System(totalElevatorNumber,totalFloorNumber);
@@ -292,6 +292,5 @@ public class Floor_System implements Runnable{
 		floorSystemThread.start();
 		elevatorSystemThread.start();
 		schedulerSystemThread.start();
-		System.exit(0); // This is to end program Only for Iteration 1 after reading data from file.
 	}
 }
