eabc1b873d91ab5f62413f1f3a7deadd35db9bcc
mfurqan26 <muhammadfurqan@cmail.carleton.ca>
2022-03-27 21:45:57 -0400

    first bug test

--- src/tests/Test_Bugs.java
+++ src/tests/Test_Bugs.java
@@ -10,18 +10,26 @@ import org.junit.Test;
 public class Test_Bugs {
 
     private Elevator_System elevator_system;
+    private Scheduler_System scheduler_system;
     private ElevatorCar elevator;
 
     @Test
-    public void test_OutOfService(){
+    /** Manually set position wrong after moving elevator and check if timing event catches the mistake
+     * and sets state to Out of Service. */
+    public synchronized void test_OutOfService(){
         this.elevator_system = new Elevator_System(1,20,false);
-        elevator_system.elevatorRunningSupportStartUp();
+
+        Thread elevatorSystemMainThread = new Thread(elevator_system , "Scheduler Simulation");
+        System.out.println("Waiting for new Task...");
+        elevatorSystemMainThread.start();
+
+        this.scheduler_system = new Scheduler_System(1,20);
         this.elevator = elevator_system.getElevators()[0];
         ArrayList<ElevatorAction> tasks = new ArrayList<>();
-        tasks.add(new ElevatorAction(5,1));
+        tasks.add(new ElevatorAction(2,1));
         elevator.setTasks(tasks);
         Float startLocation = elevator.getPosition();
-        Integer endLocation = 5;
+        Integer endLocation = 2;
 
         long time = elevator.calculateTime(startLocation,endLocation);
         elevator.moveElevator(time);
@@ -34,5 +42,4 @@ public class Test_Bugs {
         assert(elevator.getElevatorState().equals(elevator.getOutOfService()));
 
     }
-
 }
